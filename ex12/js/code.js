18e5214857816c7ff0f5fdd37ececb0ae4aceffa
Joana <joanajanefdez@gmail.com>
2022-09-05 11:03:54 +0200

    ex12

--- ex12/js/code.js
+++ ex12/js/code.js
@@ -6,3 +6,13 @@ if(regexp.test(frase)){
 }else {
     console.log("fecha mal escrita, contiene letras");
 }
+
+var mail = "joanajanefdez@gmail.com";
+var regexpMail = /[a-zA-Z1-9.-]*@{1}[a-z]*\.([a-z]{2,3})/;
+
+if(regexpMail.test(mail)){
+    console.log("email correcto");
+}else {
+    console.log("email incorrecto");
+}
+
